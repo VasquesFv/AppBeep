47579f0c3ad177a35733f515102a7013c045fe08
filipe <filipe.vasques@advances.com.br>
2019-05-10 18:44:52 -0300

    first commit

---
+++ AppBeep/app/src/main/java/com/example/filip/appbeep/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.filip.appbeep;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.widget.LinearLayout;
+import com.rd.PageIndicatorView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private SliderIntroAdapter myadapter;
+
+
+    @Override
+    protected  void onCreate (Bundle savedInstancesState){
+        super.onCreate(savedInstancesState);
+        setContentView(R.layout.activity_main);
+        viewPager = findViewById(R.id.viewpager);
+        myadapter = new SliderIntroAdapter(this);
+        viewPager.setAdapter(myadapter);
+    }
+
+
+}
---
+++ AppBeep/app/src/main/java/com/example/filip/appbeep/SliderIntroAdapter.java
@@ -0,0 +1,58 @@
+package com.example.filip.appbeep;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+
+public class SliderIntroAdapter extends PagerAdapter {
+    Context context;
+    LayoutInflater inflater;
+
+    //list of images
+
+    public int[] list_images = {
+            R.drawable.img05,
+            R.drawable.img02,
+            R.drawable.img03,
+            R.drawable.img04
+    };
+
+
+
+    public SliderIntroAdapter (Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return list_images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return (view == (LinearLayout)object);
+    }
+
+    public Object instantiateItem(ViewGroup container, int position) {
+        inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.slide,container,false);
+        LinearLayout layoutslide = (LinearLayout) view.findViewById(R.id.slidelinearlayout);
+        ImageView imgslide = (ImageView) view.findViewById(R.id.slideimage);
+        imgslide.setImageResource(list_images[position]);
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((LinearLayout)object);
+    }
+
+}
